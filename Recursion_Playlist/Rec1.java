86551c5813b2b4c5bd8f572c0028d1899ba6fe39
bhoomikaRS <bhoomika.rs@walmart.com>
2023-03-05 12:09:24 +0530

    combinationSum1

--- Recursion_Playlist/Rec1.java
+++ Recursion_Playlist/Rec1.java
@@ -1,6 +1,7 @@
 package Recursion_Playlist;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Rec1 {
@@ -137,5 +138,29 @@ public class Rec1 {
         return list;
     }
 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        getResultListForCombinationSum1(candidates, target, 0, new ArrayList<>(), ans);
+        return ans;
+    }
+
+    public void getResultListForCombinationSum1(int[] arr, int target, int idx, ArrayList<Integer> list, List<List<Integer>> ans){
+        if(idx >= arr.length){
+
+            if(target == 0){
+                ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+
+        if(arr[idx] <= target){
+            list.add(arr[idx]);
+            getResultListForCombinationSum1(arr, target - arr[idx] , idx, list, ans);
+            list.remove(list.size()-1);
+        }
+        getResultListForCombinationSum1(arr, target, idx+1, list, ans);
+
+    }
 
 }
